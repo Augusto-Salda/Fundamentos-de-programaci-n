a829c25ec0338b8a1b6e2b60d4c6e8ae17bf790c
augus <augus@DESKTOP-KREDB2H>
2020-11-12 17:28:42 -0600

    12/11/2020

---
+++ For_Tablas.c
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   newmain.c
+ * Author: augus
+ *
+ * Created on November 12, 2020, 4:18 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+
+
+int main(int argc, const char * argv[])
+{
+    //CICLOS FOR EN C
+
+    int y = 1 ;
+    int z = 1;
+    int c = 1;
+    for(int x=1;x<=10;x++){
+        printf("------------\n");
+        printf("Tabla del %d\n",x);
+        for(int c=1;c<=10;c++){
+          printf("%d X %d = %d\n",x,c, x*c );  
+        }  
+    }
+
+    return 0;
+
+}
+
+
+
