499f5f7e5d02c0f3dd23e2a147dd453eef61359c
augus <augus@DESKTOP-KREDB2H>
2020-12-24 16:53:54 -0600

    24/12/2020

---
+++ Estructura_Control_Rango_numeros.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+    int c,ni,nf;
+    printf ("Dame un número inicial de tu rango: \t");
+    scanf ("%d", &ni);   
+    printf ("Dame un número final de tu rango: \t");
+    scanf ("%d", &nf);
+    int  i = ni+1;
+    while (i<nf){
+    c= c+1;
+    i= i+1;
+}
+printf ("El rango de números que abarca es de: %d",c);
+    return 0;
+}
