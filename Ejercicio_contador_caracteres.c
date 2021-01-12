294d01f9796c2d00e73db4c7359a1466dbf75f18
augus <augus@DESKTOP-KREDB2H>
2021-01-12 17:42:29 -0600

    12/1/2021

---
+++ Ejercicio_contador_caracteres.c
@@ -0,0 +1,202 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int i2 = 0; 
+    int a=0, b=0, c=0, d=0, e=0, f=0, g=0, h=0, i=0, j=0, k=0, l=0, m=0, n=0, ñ=0, o=0, p=0, q=0, r=0, s=0, t=0, u=0, v=0, w=0, x=0, y=0, z=0;
+char arreglo[15]; 
+printf("Escibe un mensaje o cadena: \n"); 
+gets(arreglo); 
+   for(i2;i2<strlen(arreglo);i2++){
+    printf("La letra [%d] es: %c \n",i2,arreglo[i2]); 
+        switch(arreglo[i2]){
+            case 'a':
+            a++;
+            break;
+            case 'b':
+            b++;
+            break;
+            case 'c':
+            c++;
+            break;
+            case 'd':
+            d++;
+            break;
+            case 'e': 
+            e++;
+            break;
+            case 'f':
+            f++;
+            break;
+            case 'g':
+            g++;
+            break;
+            case 'h':
+            h++;
+            break;
+            case 'i':
+            i++;
+            break;
+            case 'j':
+            j++;
+            break;
+            case 'k':
+            k++;
+            break;
+            case 'l': 
+            l++;
+            break;
+            case 'm':
+            m++;
+            break;
+            case 'n':
+            n++;
+            break;
+            case 'o':
+            o++;
+            break;
+            case 'p':
+            p++;
+            break;
+            case 'q':
+            q++;
+            break;
+            case 'r': 
+            r++;
+            break;
+            case 's':
+            s++;
+            break;
+            case 't':
+            t++;
+            break;
+            case 'u':
+            u++;
+            break;
+            case 'v':
+            v++;
+            break;
+            case 'w':
+            w++;
+            break;
+            case 'x':
+            x++;
+            break;
+            case 'y':
+            y++;
+            break;
+            case 'z':        
+            z++;
+            break;
+            case 'A':
+            a++;
+            break;
+            case 'B':
+            b++;
+            break;
+            case 'C':
+            c++;
+            break;
+            case 'D':
+            d++;
+            break;
+            case 'E': 
+            e++;
+            break;
+            case 'F':
+            f++;
+            break;
+            case 'G':
+            g++;
+            break;
+            case 'H':
+            h++;
+            break;
+            case 'I':
+            i++;
+            break;
+            case 'J':
+            j++;
+            break;
+            case 'K':
+            k++;
+            break;
+            case 'L': 
+            l++;
+            break;
+            case 'M':
+            m++;
+            break;
+            case 'N':
+            n++;
+            break;
+            case 'O':
+            o++;
+            break;
+            case 'P':
+            p++;
+            break;
+            case 'Q':
+            q++;
+            break;
+            case 'R': 
+            r++;
+            break;
+            case 'S':
+            s++;
+            break;
+            case 'T':
+            t++;
+            break;
+            case 'U':
+            u++;
+            break;
+            case 'V':
+            v++;
+            break;
+            case 'W':
+            w++;
+            break;
+            case 'X':
+            x++;
+            break;
+            case 'Y':
+            y++;
+            break;
+            case 'Z':        
+            z++;
+            break;
+            
+        }  
+    }
+printf("\n [0] Letra a se encontro: %d",a);
+printf("\n [1] Letra b se encontro: %d",b);
+printf("\n [2] Letra c se encontro: %d",c);
+printf("\n [3] Letra d se encontro: %d",d);
+printf("\n [4] Letra e se encontro: %d",e);
+printf("\n [5] Letra f se encontro: %d",f);
+printf("\n [6] Letra g se encontro: %d",g);
+printf("\n [8] Letra h se encontro: %d",h);
+printf("\n [9] Letra i se encontro: %d",i);
+printf("\n [10] Letra j se encontro: %d",j);
+printf("\n [11] Letra k se encontro: %d",k);
+printf("\n [12] Letra l se encontro: %d",l);
+printf("\n [13] Letra m se encontro: %d",m);
+printf("\n [14] Letra n se encontro: %d",n);
+printf("\n [15] Letra ñ se encontro: %d",ñ);
+printf("\n [16] Letra o se encontro: %d",o);
+printf("\n [17] Letra p se encontro: %d",p);
+printf("\n [18] Letra q se encontro: %d",q);
+printf("\n [19] Letra r se encontro: %d",r);
+printf("\n [20] Letra s se encontro: %d",s);
+printf("\n [21] Letra t se encontro: %d",t);
+printf("\n [22] Letra u se encontro: %d",u);
+printf("\n [23] Letra v se encontro: %d",v);
+printf("\n [24] Letra w se encontro: %d",w);
+printf("\n [25] Letra x se encontro: %d",x);
+printf("\n [26] Letra y se encontro: %d",y);
+printf("\n [27] Letra z se encontro: %d",z);
+return 0;
+}
