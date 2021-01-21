18e40dc47357090a0e658f34a810c059a29c9112
augus <augus@DESKTOP-KREDB2H>
2021-01-21 11:03:29 -0600

    21/1/2021

---
+++ Fibonacci_Recursividad.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long fibonacci(long);
+int main(){
+long result, number;
+printf("Enter an integer: ");
+scanf ( "%ld", &number);
+result = fibonacci(number);
+printf("Fibonacci(%ld) = %ld\n", number, result);
+return 0;
+ }
+
+long fibonacci(long n){
+if (n == 0 || n == 1){
+return n;
+}else{
+return fibonacci(n - 1) + fibonacci(n - 2);
+ }
+}
